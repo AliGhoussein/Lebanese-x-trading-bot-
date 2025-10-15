fcc6cfdef9190f428785b5f7265cea28b7fd699d
AliGhoussein <alighoussein20@gmail.com>
2025-10-15 03:14:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # 🔑 حط التوكن الخاص فيك يلي عطاك ياه BotFather هون بين علامات التنصيص
-TOKEN = "8452093321:AAEkKLqr5FP_7TAwWwaKyMUdQDcD_mIMBUE"
+TOKEN = os.environ["BOT_TOKEN"]
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("أهلا وسهلا 👋! أنا Lebanese X Trading Bot، كيف فيني ساعدك اليوم؟")
 
@@ -10,4 +11,5 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 print("✅ البوت شغال... روح جرّب /start على التلغرام")
+
 app.run_polling()
