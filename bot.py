4a56016b7e75f76f7038d4981857cb99e5891465
AliGhoussein <alighoussein20@gmail.com>
2025-10-17 14:39:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from telegram.ext import (
 
 # ========== CONFIG ==========
 TOKEN = "8452093321:AAEI16NcAIFTHRt1ieKYKe1CQ1qhUfcMgjs"
-ADMIN_CHAT_ID = 1530145001
+ADMIN_CHAT_ID = 
 WHATSAPP_URL = "https://wa.me/96171204714"
 # ============================
 
@@ -251,3 +251,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
